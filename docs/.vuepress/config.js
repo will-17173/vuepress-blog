85dfea773da25a92d8a135d4e94c62e6a876317d
zhaojianhuang <zhaojian.huang@igg.com>
2021-02-09 14:04:45 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: 'simple',
+  title: 'Nothing Here',
+  description: '什么都没有',
+  evergreen: true,
+  themeConfig: {
+    author: 'Will.H',
+    navbar: {
+      什么都没有: 'https://will-17173.github.io/vuepress-blog/'
+    }
+  }
+}
